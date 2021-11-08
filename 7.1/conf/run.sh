c68d02e459dfae00f34f6645390aeeb0f4701f89
jockri <git@jockri.com>
2021-11-08 20:17:28 +0100

    added php 7.1 dockerfile + config

---
+++ 7.1/conf/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$XDEBUG" != "on" ]; then
+    sed -i -e 's/^zend_extension/\;zend_extension/g' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+fi
+
+if [[ ! -z "$USER_ID" ]]; then
+    usermod -o -u $USER_ID www-data
+fi
+
+if [[ ! -z "$GROUP_ID" ]]; then
+    groupmod -o -g $GROUP_ID www-data
+fi
+
+`$1`
